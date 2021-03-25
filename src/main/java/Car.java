b936c9bb439f19ea2b5917d0adb5d654fc147168
Andrzej Łuszcz <endriu1297@gmail.com>
2021-03-25 23:40:07 +0100

    create new class Chassis and use compose in class Car

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,15 +1,28 @@
 public class Car {
+    private Chassis chassis;
     private String firm;
     private String model;
     private String colour;
 
-    public Car(String firm, String model, String colour) {
+    public Car(Chassis chassis, String firm, String model, String colour) {
         this.firm = firm;
         this.model = model;
         this.colour = colour;
+        this.chassis = chassis;
+    }
+
+    public String getModel(){
+        return this.model;
+    }
+
+    public Chassis getChassis(){
+        return chassis;
     }
 
     public static void main(String[] args){
-        Car car = new Car("honda", "civic", "silver");
+        Car car = new Car(new Chassis(true),"honda", "civic", "silver");
+
+        System.out.println(car.getModel());
+        System.out.println(car.getChassis().getChassis());
     }
 }
---
+++ src/main/java/Chassis.java
@@ -0,0 +1,11 @@
+public class Chassis {
+    private boolean brakeSystem;
+
+    public Chassis(boolean brakeSystem) {
+        this.brakeSystem = brakeSystem;
+    }
+
+    public boolean getChassis(){
+        return brakeSystem;
+    }
+}
