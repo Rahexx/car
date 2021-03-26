1cb7621dfca18fad9ed5361ff19402bd12c9d327
Andrzej Łuszcz <endriu1297@gmail.com>
2021-03-26 18:33:00 +0100

    add class Engine with one variable power

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,16 +1,18 @@
 public class Car {
     private Chassis chassis;
     private CarBody carbody;
+    private Engine engine;
     private String firm;
     private String model;
     private String colour;
 
-    public Car(Chassis chassis,CarBody carbody, String firm, String model, String colour) {
+    public Car(Chassis chassis,CarBody carbody, Engine engine, String firm, String model, String colour) {
         this.firm = firm;
         this.model = model;
         this.colour = colour;
         this.chassis = chassis;
         this.carbody = carbody;
+        this.engine = engine;
     }
 
     public String getModel(){
@@ -23,12 +25,14 @@ public class Car {
 
     public CarBody getCarBody(){ return carbody;}
 
+    public Engine getEngine(){ return  engine; }
 
     public static void main(String[] args){
-        Car car = new Car(new Chassis(true), new CarBody("SUV"),"honda", "civic", "silver");
+        Car car = new Car(new Chassis(true), new CarBody("SUV"), new Engine("1.4"),"honda", "civic", "silver");
 
         System.out.println(car.getModel());
         System.out.println(car.getChassis().getChassis());
         System.out.println(car.getCarBody().getType());
+        System.out.println(car.getEngine().getPower());
     }
 }
---
+++ src/main/java/Engine.java
@@ -0,0 +1,11 @@
+public class Engine {
+    private String power;
+
+    public Engine(String power) {
+        this.power = power;
+    }
+
+    public String getPower() {
+        return power;
+    }
+}
